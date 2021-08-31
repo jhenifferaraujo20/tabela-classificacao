16400509c46c474a4e1a0d7de53771c685789f2f
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2021-08-31 20:59:39 -0300

    Função de calcular e exibir os pontos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,56 @@
+let pontosCruzeiro = 0, pontosFlamengo = 0;
+
 let btnVitoriaCruzeiro = document.querySelector("#vitoria-cruzeiro");
+let btnVitoriaFlamengo = document.querySelector("#vitoria-flamengo");
+let btnEmpateCruzeiro = document.querySelector("#empate-cruzeiro");
+let btnEmpateFlamengo = document.querySelector("#empate-flamengo");
+let btnDerrotaCruzeiro = document.querySelector("#derrota-cruzeiro");
+let btnDerrotaFlamengo = document.querySelector("#derrota-flamengo");
 
 btnVitoriaCruzeiro.onclick = () => {
-    console.log("Derrota Cruzeiro");
+    pontosCruzeiro += (CalcularPontos("vitória"));
+    AtualizarTabelaPontos("cruzeiro");
 };
 
-let btnVitoriaFlamengo = document.querySelector("#vitoria-flamengo");
+btnVitoriaFlamengo.onclick = () => {
+    pontosFlamengo += (CalcularPontos("vitória"));
+    AtualizarTabelaPontos("flamengo");
+};
 
-btnVitoriaFlamengo.addEventListener('click', () => {
-    console.log("Derrota Flamengo")
-});
+btnEmpateCruzeiro.onclick = () => {
+    pontosCruzeiro += (CalcularPontos("empate"));
+    AtualizarTabelaPontos("cruzeiro");
+};
+
+btnEmpateFlamengo.onclick = () => {
+    pontosFlamengo += (CalcularPontos("empate"));
+    AtualizarTabelaPontos("flamengo");
+};
+
+btnDerrotaCruzeiro.onclick = () => {
+    pontosCruzeiro += (CalcularPontos("derrota"));
+    AtualizarTabelaPontos("cruzeiro");
+};
+
+btnDerrotaFlamengo.onclick = () => {
+    pontosFlamengo += (CalcularPontos("derrota"));
+    AtualizarTabelaPontos("flamengo");
+};
+
+function CalcularPontos(resultado){
+    if(resultado == "vitória") {
+        return 3;
+    }else if(resultado == "empate") {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+function AtualizarTabelaPontos(time) {
+    if(time == "cruzeiro"){
+        document.querySelector(".pontos-cruzeiro").innerHTML = pontosCruzeiro;
+    }else if(time == "flamengo"){
+        document.querySelector(".pontos-flamengo").innerHTML = pontosFlamengo;
+    }
+}
